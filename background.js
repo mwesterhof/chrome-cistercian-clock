09841107d768d13e1ee327391bb45c5e9eb49736
Marco Westerhof <westerhof.marco@gmail.com>
2025-07-18 17:07:35 +0200

    icons and tweaks

--- background.js
+++ background.js
@@ -1,22 +1,37 @@
+const ICONS = {
+  "default": {
+    "16": "images/default/icon-16.png",
+    "32": "images/default/icon-32.png",
+    "48": "images/default/icon-48.png",
+    "128": "images/default/icon-128.png"
+  },
+  "active": {
+    "16": "images/active/icon-16.png",
+    "32": "images/active/icon-32.png",
+    "48": "images/active/icon-48.png",
+    "128": "images/active/icon-128.png"
+  }
+}
+
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "OFF",
-  });
+  chrome.storage.local.set({ isActive: false });
+  chrome.action.setIcon({ path: ICONS.default });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  // Next state will always be the opposite
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  const storedState = await chrome.storage.local.get('isActive');
+  const state = !storedState.isActive;
 
-  // Set the action badge to the next state
-  await chrome.action.setBadgeText({
-    tabId: tab.id,
-    text: nextState,
-  });
+  if (state) {
+    chrome.action.setIcon({ path: ICONS.active });
+  }
+  else {
+    chrome.action.setIcon({ path: ICONS.default });
+  }
 
-  console.log('blah');
+  chrome.storage.local.set({ isActive: state });
+
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['content.js']
